14d53cf25edcc3515f4f60e6890ce6f4e415f185
Ulver <jculver@gmail.com>
2023-04-24 16:49:50 +0200

    Creacion de metodos addProductos

---
+++ src/empresa/empresaAlimentos/EmpresaAgroalimentaria.java
@@ -0,0 +1,30 @@
+package empresa.empresaAlimentos;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaAgroalimentaria {
+    String nombre;
+    List <Productos> listaProductos;
+    List <ProductosFrescos> listaFrescos;
+    List <ProductosRefrigerados> listaRefrigerados;
+    List <ProductosCongelados> listaCongelados;
+
+    public EmpresaAgroalimentaria(String nombre) {
+        this.nombre = nombre;
+        this.listaProductos = new ArrayList<>();
+        this.listaFrescos = new ArrayList<>();
+        this.listaRefrigerados = new ArrayList<>();
+        this.listaCongelados = new ArrayList<>();
+      }
+    
+    public void addProductosFrescos(ProductosFrescos productoFresco){
+        listaFrescos.add(productoFresco);
+    }
+    public void addProductosRefrigerados(ProductosRefrigerados productoRefrigerado){
+        listaRefrigerados.add(productoRefrigerado);
+    }
+    public void addProductosCongelados(ProductosCongelados productoCongelado){
+        listaCongelados.add(productoCongelado);
+    }
+
+}

a5e0a8faa99a54dd1269c91cfdb5eb53185d4552
ulvercalleja <ulver.calleja@educa.madrid.org>
2023-04-27 19:55:21 +0200

    commit

--- src/BuscaminasMain.java
+++ src/BuscaminasMain.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import juegos.buscaminas.*;
+
 public class BuscaminasMain {
   public static void main(String[] args) {
     Scanner leer = new Scanner(System.in);
@@ -17,42 +18,43 @@ public class BuscaminasMain {
       System.out.println("3. Salir 3");
       opcion = leer.nextInt();
 
-      //Comprobamos que el jugador elige una opcion correcta
+      // Comprobamos que el jugador elige una opcion correcta
       while (opcion < 1 || opcion > 3) {
         System.out.println("Por favor, seleccione un numero entre el 1 y el 3.");
         opcion = leer.nextInt();
       }
       switch (opcion) {
         case 1:
-        System.out.println("Usted ha seleccionado jugar.");
+          System.out.println("Usted ha seleccionado jugar.");
 
-        //Inicializamos casillas y generamos minas ANTES DEL BUCLE.
-        consBuscaminas.inicializarCasillas();
-        consBuscaminas.generarMinas();
+          // Inicializamos casillas y generamos minas ANTES DEL BUCLE.
+          consBuscaminas.inicializarCasillas();
+          consBuscaminas.generarMinas();
 
-        while (true) {
-          
-          //Mostramos el tablero y le pedimos posiciones para revelar casilla
-          consBuscaminas.mostrarTablero();
+          while (true) {
 
-          if (consBuscaminas.comprobarVictoria()) {
-            System.out.println("¡Has ganado!");
-            break;
-          }
-          
-          System.out.print("Introduce fila: ");
-          int fila = leer.nextInt();
-          System.out.print("Introduce columna: ");
-          int columna = leer.nextInt();
-          
-          consBuscaminas.descubrirCasilla(fila, columna);
+            // Mostramos el tablero y le pedimos posiciones para revelar casilla
+            consBuscaminas.mostrarTablero();
+
+            if (consBuscaminas.comprobarVictoria()) {
+              System.out.println("¡Has ganado!");
+              break;
+            }
+
+            System.out.print("Introduce fila: ");
+            int fila = leer.nextInt();
+            System.out.print("Introduce columna: ");
+            int columna = leer.nextInt();
+
+            consBuscaminas.descubrirCasilla(fila, columna);
             // se verifica si la casilla seleccionada tiene una mina
             if (consBuscaminas.tablero[fila][columna].getEsMina()) {
               System.out.println("¡Has perdido! Has descubierto una mina.");
             } else {
-              consBuscaminas.descubrirCasilla(fila, columna); // se descubre la casilla y se muestra el número de minas adyacentes
+              consBuscaminas.descubrirCasilla(fila, columna); // se descubre la casilla y se muestra el número de minas
+                                                              // adyacentes
             }
-        }
+          }
 
         case 2:
           System.out.println("Usted ha seleccionado opciones.");
@@ -67,6 +69,7 @@ public class BuscaminasMain {
       }
     } while (opcion != 3);
 
+    leer.close();
     System.out.println("¡Vuelva pronto!");
   }
 }

9b707d5fa47c5988440c8b251918802a8bd43365
ulvercalleja <ulver.calleja@educa.madrid.org>
2023-04-27 18:33:28 +0200

    Commit

--- src/MainEmpresaAgroalimentaria.java
+++ src/MainEmpresaAgroalimentaria.java
@@ -4,12 +4,11 @@ public class MainEmpresaAgroalimentaria {
   public static void main(String[] args) {
     EmpresaAgroalimentaria empresa = new EmpresaAgroalimentaria("AgroUlver");
 
-    // Crea un objeto de la clase ProductosFrescos y lo inicializa utilizando el
-    // constructor
+    // Crea un objeto de la clase (ProductosFrescos) y lo inicializa utilizando el constructor
     ProductosFrescos fresco = new ProductosFrescos("tomate", "2023-05-30", "ES202317", "ES", "2023-04-18");
     empresa.addProductosFrescos(fresco);
 
-    // Crea otro objeto fresco con diferentes valores
+    // Crea otro objeto con diferentes valores
     fresco = new ProductosFrescos("lechuga", "2023-06-30", "ES202335", "FR", "2023-04-15");
     empresa.addProductosFrescos(fresco);
 
@@ -29,19 +28,19 @@ public class MainEmpresaAgroalimentaria {
         "FR", "2023-04-25", 3, 4);
     empresa.addProductosCongelados(congeladosPorAgua);
 
-    congeladosPorAgua = new CongeladosPorAgua("Guisante Congelados", "2023-08-19", "ES76831",
+    congeladosPorAgua = new CongeladosPorAgua("Guisantes Congelados", "2023-08-19", "ES76831",
         "IT", "2023-04-21", 4, 3);
     empresa.addProductosCongelados(congeladosPorAgua);
 
-    CongeladosPorAire congeladosPorAire = new CongeladosPorAire("Guisante Congelados", "2023-09-10", "ES827931",
+    CongeladosPorAire congeladosPorAire = new CongeladosPorAire("Helado de Vainilla", "2023-09-10", "ES827931",
         "IT", "2023-05-26", 3, 3, 4, 3 ,4);
     empresa.addProductosCongelados(congeladosPorAire);
 
-    congeladosPorAire = new CongeladosPorAire("Guisante Congelados", "2023-07-15", "ES823431",
+    congeladosPorAire = new CongeladosPorAire("Merluza", "2023-07-15", "ES823431",
         "ES", "2023-04-21", 3, 4, 5, 3 ,4);
     empresa.addProductosCongelados(congeladosPorAire);
 
-    CongeladosPorNitro congeladosPorNitro = new CongeladosPorNitro("Guisante Congelados", "2023-09-10", "ES827931",
+    CongeladosPorNitro congeladosPorNitro = new CongeladosPorNitro("Pollo", "2023-09-10", "ES827931",
         "IT", "2023-05-26", 3, "?","1 semana");
     empresa.addProductosCongelados(congeladosPorNitro);
 
--- src/empresa/empresaAlimentos/EmpresaAgroalimentaria.java
+++ src/empresa/empresaAlimentos/EmpresaAgroalimentaria.java
@@ -5,14 +5,12 @@ import java.util.List;
 
 public class EmpresaAgroalimentaria {
   String nombre;
-  List<Productos> listaProductos;
-  List<ProductosFrescos> listaFrescos;
-  List<ProductosRefrigerados> listaRefrigerados;
-  List<ProductosCongelados> listaCongelados;
+  List <ProductosFrescos> listaFrescos;
+  List <ProductosRefrigerados> listaRefrigerados;
+  List <ProductosCongelados> listaCongelados;
 
   public EmpresaAgroalimentaria(String nombre) {
     this.nombre = nombre;
-    this.listaProductos = new ArrayList<>();
     this.listaFrescos = new ArrayList<>();
     this.listaRefrigerados = new ArrayList<>();
     this.listaCongelados = new ArrayList<>();
